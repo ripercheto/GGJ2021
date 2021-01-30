9776987f304ca51150e44545c32daa4cc57acf01
robbie932 <r.mcdonald@live.nl>
2021-01-30 18:44:07 +0100

    Satisfaction UI + Item request UI
    Added item icons
    Added satisfaction icons
    Added QuestBehaviour script
    Added item scriptableObjects

---
+++ Assets/_Code/Item.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New item", menuName = "Item")]
+public class Item : ScriptableObject
+{
+    new public string name = "New item";
+    public Sprite icon = null;
+
+}
